00289caf630c3bfaac33ddcf98ef5687ccb47077
HappyDevs1 <153817052+HappyDevs1@users.noreply.github.com>
2024-05-26 23:00:24 +0200

    minor changes

--- to-do-app/src/App.js
+++ to-do-app/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import TodoList from './components/TodoList'
+import TodoItem from './components/TodoItem'
 
 function App () {
   return (
     <div>
-      <h1>To-Do-APP</h1>
+      <h1>To-Do-App</h1>
       <TodoList />
     </div>
   )
